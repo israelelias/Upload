575b29e7d03045d61bd5384983222cc014d84b34
israelelias <israelelias2@gmail.com>
2015-11-19 11:45:01 -0200

    correções

--- Uploaded.php
+++ Uploaded.php
@@ -13,10 +13,8 @@ class Uploaded {
 
 	public function moveFile( File $file ) {
 		$filename = new DrawFilename();
-		$path = $this->path . '/' . $filename->draw($file);
-		$erro = move_uploaded_file($file->getTmp_name(), $path) ? 1 : 0 ;
 		
-		if ($erro === 0) {
+		if (move_uploaded_file($file->getTmp_name(), $this->path . '/' . $filename->draw($file))) {
 			throw new Exception("Não foi possível enviar o arquivo, tente novamente");		
 		}
 	}
